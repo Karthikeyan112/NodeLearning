79a3d5e5f687805c16a433d0b13aa1a65be77031
Karthikeyan Krishinamoorthy - k0k078n <karthikeyan.krishinamoorthy@walmart.com>
2023-12-21 22:20:56 +0530

    smaple get api

---
+++ index.js
@@ -0,0 +1,21 @@
+const fastify = require('fastify')({
+  logger: true
+});
+
+// Declare Route
+fastify.get("/",async (req, reply) => {
+  console.log(req.query);
+  const {name} = req.query;
+  reply.status(200).send(`Hello ${name}`);
+});
+
+// Run the server
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 });
+  }catch(err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+}
+start();
